9f59ff0831d16d3cad889b672de184f4f029b026
thomas School <thomas School>
2017-03-26 13:05:35 -0600

    inital commit

---
+++ Assets/code test.cs
@@ -0,0 +1,36 @@
+﻿/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class codetest : MonoBehaviour {
+	public bool goal = false;
+	public int speed = 10;
+	public int strength;
+	public int cnt = 0;
+	//this code contains the information of objects
+	public struct objects
+	{
+		public int x;
+		public int y;
+		/*
+	sprite 
+	*/
+/*
+		public int height;
+		public int width;
+	}
+	//this declares the player
+	public struct objects player;
+	// Use this for initialization
+	void Start () {
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+*/
---
+++ Assets/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+	float x;
+	float y;
+	public float speed;
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		y = transform.position.y;
+		x = transform.position.x;
+		//Debug.Log (x);
+		if (Input.GetKey(KeyCode.D) == true) 
+		{
+		//	Debug.Log ("hi");
+			transform.position = new Vector2(x + speed, y);
+		//	Debug.Log("there");
+		}
+		if (Input.GetKey (KeyCode.A) == true) 
+		{
+			transform.position = new Vector2 (x - speed, y);
+		}
+	}
+}
